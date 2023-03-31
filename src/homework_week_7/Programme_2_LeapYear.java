64b0ef9701f4e0d42ae6b1220a385844c1019b52
paresha panchal <primetraineesix@gmail.com>
2023-03-31 15:51:00 +0100

    My week3 writting homework

---
+++ src/homework_week_7/Programme_2_LeapYear.java
@@ -0,0 +1,27 @@
+package homework_week_7;
+import java.util.Scanner;
+/**
+ * Write a java program to input any year like(ex. 200
+ */
+public class Programme_2_LeapYear {
+    public static void main(String[] args){
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the Year: ");
+        int year = scanner.nextInt();
+        Programme_2_LeapYear leapYear = new Programme_2_LeapYear();
+        leapYear.isItLeapYear(year);
+        //closing the scanner object
+        scanner.close();
+    }
+    //checking is it leap year or not
+    public void isItLeapYear(int year){
+        if(year %4 ==0 && year%100 !=0 || year %400 ==0)
+        {
+            System.out.println("This year "+ year + " is leap year");
+            return;
+        }
+        System.out.println("The year "+ year + " is not a leap year ");
+    }
+
+}
